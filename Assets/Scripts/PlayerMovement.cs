adb8baf621799a2ac1f28cfa6f6101588a89fde1
Harald-R <razvan.harald@yahoo.com>
2018-12-27 21:14:41 +0200

    Add support for touch input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,32 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public float runSpeed = 40f;
+    public Joystick joystick;
+    public JumpButton jumpButton;
 
     private float horizontalMove = 0f;
     private bool jump = false;
 
-    // Update is called once per frame
     void Update()
     {
+    #if UNITY_STANDALONE // || UNITY_EDITOR
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         if(Input.GetButtonDown("Jump")) {
             jump = true;
         }
+    #elif UNITY_ANDROID
+        if(Mathf.Abs(joystick.Horizontal) < .2f) {
+            horizontalMove = 0f;
+        } else if(Mathf.Abs(joystick.Horizontal) < .5f) {
+            horizontalMove = Mathf.Sign(joystick.Horizontal) * ScaleToRange(Mathf.Abs(joystick.Horizontal), .2f, .5f, runSpeed * .5f, runSpeed);
+        } else {
+            horizontalMove = Mathf.Sign(joystick.Horizontal) * runSpeed;
+        }
+
+        if(jumpButton.isPressed) {
+            jump = true;
+        }
+    #endif
     }
 
     void FixedUpdate()
@@ -24,5 +39,10 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
 
         jump = false;
+    }
+
+    float ScaleToRange(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 }
---
+++ Assets/Scripts/Touch Input/JumpButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JumpButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public RectTransform buttonImage;
+    public Color pressedColor;
+
+    [System.NonSerialized] public bool isPressed = false;
+
+    private Image image;
+    private Color defaultColor;
+
+    void Start()
+    {
+        image = buttonImage.GetComponent<Image>();
+        defaultColor = image.color;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+        image.color = pressedColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+        image.color = defaultColor;
+    }
+}

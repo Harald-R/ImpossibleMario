2fe96e6ef996a523c865da6f9cab6fc7724d7516
raulsteleac <raulsteleac@yahoo.com>
2019-01-10 01:13:33 +0200

    Lul

---
+++ Assets/Scripts/Env/AttachPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AttachPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+            {
+                player.transform.parent = transform;
+            }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+            {
+                player.transform.parent = null;
+            }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+            player = GameObject.Find("Player(Clone)");
+    }
+}

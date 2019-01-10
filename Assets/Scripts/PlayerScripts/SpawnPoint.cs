471e8de61401486f400fb7532234d1b2f1d8c109
Andrei <andreirosu97@gmail.com>
2019-01-10 22:26:28 +0200

    Falling

---
+++ Assets/Scripts/PlayerScripts/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Spawn()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+
+    void SetPrefab(GameObject newPrefab)
+    {
+        prefab = newPrefab;
+    }
+
+}

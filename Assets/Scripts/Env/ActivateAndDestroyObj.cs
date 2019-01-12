917a10078e0debe8b565154e0151ba9de450f46e
raulsteleac <raulsteleac@yahoo.com>
2019-01-12 02:22:03 +0200

    Hallucination

---
+++ Assets/Scripts/Env/ActivateAndDestroyObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ActivateAndDestroyObj : MonoBehaviour {
+ 
+ 	public GameObject player;
+	private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject == player)
+            {
+                WaitForSecs(3);
+        		gameObject.SetActive(false);
+            }
+    }
+
+ 
+    IEnumerator WaitForSecs (int seconds){    	
+        yield return new WaitForSeconds(seconds);
+    }
+
+    void Update()
+    {
+    	if (player == null)
+        {
+            if(GameObject.FindGameObjectsWithTag("Player").Length > 0)
+                player = GameObject.FindGameObjectsWithTag("Player")[0];
+        }
+    }
+}
+ 

15b263a1f6fa7bc103aef6d417762be1eccd5e74
Andrei <andreirosu97@gmail.com>
2019-01-13 23:37:01 +0200

    Revert "Error delete"
    This reverts commit 01e690a73c8879ab7d73d88d56000cf99edb0dd3.

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    public GameObject spawnPoint = null;
+
+    void Awake()
+    {
+        StartHost();
+        Debug.Log("Awake");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Debug.Log("OnServerAddPlayer");
+        spawnPoint = GameObject.FindGameObjectWithTag("Respawn");
+        GameObject player = (GameObject)Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("OnClientConnet");
+        ClientScene.AddPlayer(conn, 0);
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("OnClientDisconnect");
+        base.OnClientDisconnect(conn);
+    }
+    public override void OnStopHost()
+    {
+        Debug.Log("OnStopHost");
+        spawnPoint = null;
+        base.OnStopHost();
+    }
+
+}

30493bec466509f2735fda6f14b29ed018b01f9e
Harald-R <razvan.harald@yahoo.com>
2019-01-18 09:21:01 +0200

    Add pause menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Awake()
+    {
+        if(pauseMenu == null) {
+            pauseMenu = GameObject.Find("PauseMenu");
+        }
+    }
+
+    public void TogglePauseMenu()
+    {        
+        if(pauseMenu.gameObject.activeInHierarchy) {
+            pauseMenu.gameObject.SetActive(false);
+            // Time.timeScale = 1f;
+        } else {
+            pauseMenu.gameObject.SetActive(true);
+            // Time.timeScale = 0f;
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        // TODO: Do the server disconnect logic
+        SceneManager.LoadScene("MainMenu");
+    }
+}

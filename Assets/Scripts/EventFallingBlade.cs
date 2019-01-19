090543c72415505c4373f08073d56064b07f039e
raulsteleac <raulsteleac@yahoo.com>
2019-01-19 23:16:06 +0200

    Added blades

---
+++ Assets/Scripts/EventFallingBlade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFallingBlade :  MovingObject, IEventTrigger 
+{
+    private int eventCounter;
+    private bool triggerEventFlag = false;
+    public GameObject[] Subjects;
+
+    protected override bool is_ConditionSatisfied()
+    {
+        return triggerEventFlag;
+    }
+
+    public void TriggerEvent()
+    {
+        Debug.Log("EnableColider");
+        transform.GetComponent<BoxCollider2D>().enabled = triggerEventFlag;
+    }
+    
+    public void NotifyChange()
+    {
+        Debug.Log("NotifyChange");
+        foreach (GameObject subject in Subjects)
+        {
+            triggerEventFlag = triggerEventFlag || subject.GetComponent<IEventSubject>().GetState();
+            Debug.Log(subject.GetComponent<IEventSubject>().GetState());
+        }
+
+        if (triggerEventFlag)
+            TriggerEvent();
+
+        triggerEventFlag = true;
+    }
+}
